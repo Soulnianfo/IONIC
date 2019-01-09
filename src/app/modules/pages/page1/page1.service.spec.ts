59c3b86110e2f2120291ab5f84600972c6de283c
Mickael FAUST <mickael.faust@gmail.com>
2019-01-09 09:31:01 +0100

    First service

---
+++ src/app/modules/pages/page1/page1.service.spec.ts
@@ -0,0 +1,16 @@
+import {Page1Service} from "./page1.service";
+
+
+describe('Service: List Service', () => {
+  let service: Page1Service;
+
+
+  beforeEach(()=> {
+      service = new Page1Service();
+    }
+  );
+
+  it('should test', () => {
+
+  });
+});
---
+++ src/app/modules/pages/page1/page1.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from "@angular/core";
+
+@Injectable({
+    providedIn: "root"
+  }
+)
+export class Page1Service {
+
+  constructor(){}
+
+  public monService(){
+
+  }
+
+}
