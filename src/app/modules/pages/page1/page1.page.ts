9d6c73a2ea85866999480a20ace7adb766f4202f
Mickael FAUST <mickael.faust@gmail.com>
2019-01-14 14:59:06 +0100

    Apple d'un ws et affichage

--- src/app/modules/pages/page1/page1.page.ts
+++ src/app/modules/pages/page1/page1.page.ts
@@ -1,18 +1,29 @@
 import {Component, OnInit} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Page1Service} from "./page1.service";
 
 @Component({
   selector: 'page1',
   templateUrl: './page1.page.html',
   styleUrls: ['./page1.page.scss'],
   host: {'class': 'page1'},
-  providers: []
+  providers: [Page1Service]
 })
 export class Page1Page implements OnInit {
 
-  constructor() {}
+  public articles: Array<any>;
+
+  constructor(public page1Service: Page1Service) {}
 
   ngOnInit() {
     console.log("INIT PAGE1");
+
+    this.page1Service.getArticles().subscribe(
+
+      (data: Array<any>) => {
+        this.articles = data;
+      }
+    );
   }
 }
 
--- src/app/modules/pages/page1/page1.service.ts
+++ src/app/modules/pages/page1/page1.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AppService {
+@Injectable()
+export class Page1Service {
 
 
   constructor(public httpclient: HttpClient) {
+  }
+
+  getArticles(){
+    return this.httpclient.get("https://jsonplaceholder.typicode.com/posts");
   }
 
 }
